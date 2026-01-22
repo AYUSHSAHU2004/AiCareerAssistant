45e2f675199f058deb225c5ee7664f431f4a1563
Ayush Sahu <2004ayushsahu@gmail.com>
2026-01-22 18:29:49 +0530

    feat : made redis email queue

--- emailSender/api-server/api-server.js
+++ emailSender/api-server/api-server.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(cors());        app.use(express.json());
 
 
+const emailQueue = new Queue('emailQueue', {
+  redis: { host: process.env.REDIS_HOST, port: process.env.REDIS_PORT },
+});
 
 
 
